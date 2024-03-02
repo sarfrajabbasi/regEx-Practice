3c9dd8a80161429eefbc751fc9ea2c3cf7a8d567
sarfrajabbasi <sarfrajk96811@gmail.com>
2024-03-02 17:10:06 +0530

    regEx practice in form

--- daneil-sifferman/js/regEx.js
+++ daneil-sifferman/js/regEx.js
@@ -42,16 +42,50 @@ sub gp --> $2
 var r = /^\d{3}$/;
 var pattern = {
     matchMe:/\b[a-z]+\b/gi,
-    matchPhone:/(\d{3})[-.]\d{4}/
+    matchPhone:/(\d{3})[-.]\d{4}/,
+    splitMe:/(\W+)/
 }
 
 console.log(r.test("123"));
 
-var s = "Me and My Problems With Logics and data structred";
+var s = "coding Challenges and word interactor with regular expressions and Programming with Text";
 var p = "123-1234 and 345-4444 and 567-7777";
 
+// match
 const result = s.match(pattern.matchMe)
 const result2 = p.match(pattern.matchPhone);
+var result3;
+
+// exec
+// while(result3 = pattern.matchPhone.exec(p)){
+//     console.log(result3);
+    
+// }
+
+// split
+const matchMe = s.split(pattern.splitMe) 
+
+
+// replace
+const rep = s.replace(/([aeiou])/g,"$1$1")
+const rep1 = p.replace(/(\d{3})-(\d{4})/g,replacer)
+function replacer(match,group1,group2){
+    console.log(match);
+    console.log(group1);
+    console.log(group2);
+    console.log(arguments);
+    if(match.length == 1){
+     
+    }else{
+        
+        return match;
+    }
+}
+
 
 console.log(result);
 console.log(result2);
+// console.log(result3);
+// console.log(matchMe);
+console.log(rep);
+console.log(rep1);
